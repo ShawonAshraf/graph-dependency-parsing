db1701d13470fe433a57631ab93f6be046a2c15d
Shawon Ashraf <shawon13@live.com>
2023-02-02 19:53:01 +0100

    str rep

--- src/data/sentence.py
+++ src/data/sentence.py
@@ -3,7 +3,13 @@ from typing import List
 
 from .conll06_token import Conll06Token
 
-
 @dataclass
 class Sentence:
     tokens: List[Conll06Token]
+
+    def __str__(self) -> str:
+        s = ""
+        for token in self.tokens:
+            s += str(token)
+
+        return s + "\n"
