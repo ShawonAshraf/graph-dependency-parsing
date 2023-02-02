429ee9cabf8ff1729e1c630c7c6e5a2a2ea71017
Shawon Ashraf <shawon13@live.com>
2023-02-02 19:08:55 +0100

    create str rep

--- src/data/conll06_token.py
+++ src/data/conll06_token.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Any
 
+
 @dataclass
 class Conll06Token:
     _id: Any
@@ -19,3 +20,16 @@ class Conll06Token:
     def __post_init__(self):
         self._id = int(self._id)
         self.head = int(self.head)
+
+    # create a representation like the conllu06 format
+    def __str__(self):
+        attributes = self.__dict__
+        s = ""
+        for attrib, value in attributes.items():
+            # check if a token property has new line
+            v = str(value)
+            if "\n" in v:
+                s += v
+            else:
+                s += v + "\t"
+        return s
