d128d5a389494f489306a9efcd6d5f2d2ddf8219
Shawon Ashraf <shawon13@live.com>
2023-02-02 16:49:11 +0100

    post init conversion

--- src/data/conll06_token.py
+++ src/data/conll06_token.py
@@ -1,15 +1,21 @@
 from dataclasses import dataclass
-
+from typing import Any
 
 @dataclass
 class Conll06Token:
-    _id: int
+    _id: Any
     form: str
     lemma: str
     pos: str
     xpos: str
     morph: str
-    head: int
+    head: Any
     rel: str
     placeholder_1: str
     placeholder_2: str
+
+    # convert str values of _id and head to int
+    # the file reader return strings
+    def __post_init__(self):
+        self._id = int(self._id)
+        self.head = int(self.head)
