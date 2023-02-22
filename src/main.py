6be15337c2fdf4dab79340da6665e97354a61f47
ShawonAshraf <shawon13@live.com>
2023-02-22 17:03:22 +0100

    derp

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import json
 
 from data.io import read_conll06_file
-from model.eval import las, uas
-from model.features import preprocess, create_vector_representation, encode_labels
+from model.features import preprocess
 from model.perceptron import Perceptron
+from graph.decoder import construct_and_decode
+from model.parser import Parser
 
 # read configs from a json file for now
 # replace later with argparse
@@ -12,35 +13,15 @@ with open(config_file_path, "r") as json_file:
     config = json.load(json_file)
 
 if __name__ == "__main__":
-    # sentences = read_conll06_file(config["file"])
-    # write to file
-    # out_file_path = "../out.conll06"
-    # write_conll06_file(sentences, out_file_path)
-    gold = read_conll06_file(config["eval_gold"])
-    pred = read_conll06_file(config["eval_pred"])
-
     train = read_conll06_file(config["train"])
-
-    uas_score = uas(gold, pred)
-    print(uas_score)
-
-    las_score = las(gold, pred)
-    print(las_score)
 
     fdict, fs = preprocess(train)
     p = Perceptron(fdict, normalise=True)
-    p.train(1, fs)
+    # p.train(1, fs)
     # p.save("./pickle.out")
     # p.load("./pickle.out")
     # print(p.weights)
 
-    # print(create_vector_representation(fdict, fs))
-    # print(fs[1])
-
-    # rep = create_vector_representation(fdict, fs)
-    # lab = encode_labels(fs)
-    # print(rep[0])
-
-    # print(fdict)
-    # print(gold[23])
-    # extract_feature(gold[23])
+    parser = Parser(p, construct_and_decode)
+    heads = parser.parse(fs[0].features)
+    print(heads)
