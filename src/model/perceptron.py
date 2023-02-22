b8d37e95bd1b01a2c00166646cfbdfc06d41e376
ShawonAshraf <shawon13@live.com>
2023-02-22 04:41:55 +0100

    train and update

--- src/model/perceptron.py
+++ src/model/perceptron.py
@@ -25,28 +25,64 @@ class Perceptron:
     # for one sentence
     # creates and returns the score matrix
     # based on weights
-    def forward(self, sentence_features):
+    def forward(self, sentence_features) -> np.ndarray:
         # the adjacency matrix for the graph should be
         # a square matrix
-        # all tokens against each other
-        score_matrix = np.array(shape=(
-            len(sentence_features),
-            len(sentence_features)
+        # rows are heads
+        # cols are dependents
+        # idx 0,0 is for root
+        score_matrix = np.ones(shape=(
+            len(sentence_features) + 1,
+            len(sentence_features) + 1
         ))
+        score_matrix *= -np.Inf  # default value
 
-        for i, token_features in enumerate(sentence_features):
-            for j, tokf in enumerate(token_features):
-                if tokf in self.weights.keys():
-                    score_matrix[i][j] += self.weights[tokf]
+        # length of the sentences features
+        s = len(sentence_features)
+
+        # again, rows are heads
+        for i in range(s):
+            # features for the current token in the sentence
+            current_token_features = sentence_features[i]
+
+            # cols are dependents
+            for j in range(i + 1, s):
+                total = 0.0
+                for tokf in current_token_features:
+                    if tokf in self.weights.keys():
+                        total += self.weights[tokf]
+
+                # add the total to the score matrix
+                score_matrix[i][j] = total
 
         return score_matrix
 
-    def train(self, epochs):
+    def train(self, epochs, processed_instances: List[ProcessedInstance]):
         for e in trange(epochs):
-            pass
+            for _, pi in enumerate(processed_instances):
+                sentence_features = pi.features
+                labels = pi.labels
 
-    def predict(self):
-        pass
+                # actual heads
+                targets = [l[1] for l in labels]
+
+                score_matrix = self.forward(sentence_features)
+
+                # square matrix so shape index doesn't matter
+                for idx, target in enumerate(targets):
+                    predicted_head_idx = np.argmax(score_matrix[:, idx])
+                    target_idx = target - 1
+
+                    # update weights
+                    factor = 1.0 if predicted_head_idx == target_idx else -1.0
+                    self.update(factor, sentence_features)
+
+    # update the corresponding token feature weights
+    def update(self, factor: float, sentence_features: List[List[str]]) -> None:
+        for token_features in sentence_features:
+            for tokf in token_features:
+                if tokf in self.weights.keys():
+                    self.weights[tokf] += factor
 
     def save(self):
         pass
